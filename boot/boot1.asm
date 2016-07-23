d7589a3f71dcdeff436fd338588e906d4a27debd
J. Zerg <jsmartini.accounts@gmx.com>
2016-07-23 02:34:23 +0000

    Create boot1.asm

---
+++ boot/boot1.asm
@@ -0,0 +1,12 @@
+org	0x7c00				          ; We are loaded by BIOS at 0x7C00
+ 
+bits	16					            ; We are still in 16 bit Real Mode
+ 
+Start:
+ 
+	cli					                ; Clear all Interrupts
+	hlt					                ; halt the system
+	
+times 510 - ($-$$) db 0				; We have to be 512 bytes. Clear the rest of the bytes with 0
+ 
+dw 0xAA55					            ; Boot Signiture
